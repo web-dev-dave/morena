460cf0f3cb04772a8f720a5ceb8b1a13813d75f2
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-10-29 20:26:51 +1300

    added table migration

--- seeds/english_quotes.js
+++ seeds/english_quotes.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('english').insert([
-        {id: 1, author: 'rowValue1', quote:''},
-        {id: 2, author: 'rowValue2', qoute:''},
-        {id: 3, author: 'rowValue3', quote:''}
+        {id: 1001, author: 'Jerry Rice', quote:'Today I will do what others won’t, so tomorrow I can do what others can’t'},
+        {id: 1002, author: 'Billie Jean King', qoute:'Pressure is a privilege – it only comes to those who earn it.'},
+        {id: 1003, author: 'Winnie the Pooh', quote:'You’re braver than you believe, stronger than you seem, and smarter than you think'}
       ]);
     });
 };
