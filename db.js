56aa9bbce04edbe077d15cea8a2d1afe792b1f4f
caroline-pitcher <70181184+caroline-pitcher@users.noreply.github.com>
2020-10-30 10:53:30 +1300

    Adding functions for get quote route and corresponding db functions

--- db.js
+++ db.js
@@ -1,3 +1,16 @@
 const knex = require('knex')
 const config = require('./knexfile').development
 const database = knex(config)
+
+module.exports = {
+    getEnglish,
+    getTeReo
+}
+
+function getEnglish (db = connection) {
+
+}
+
+function getTeReo (db = connection) {
+    
+}
--- routes.js
+++ routes.js
@@ -16,5 +16,14 @@ router.get('/', (req, res) => {
 
 //  Render for quote route aka quoteview.hbs
   router.get('/quote', (req, res) => {
-    res.render('quoteview')
+    // getting a quote on the page using the id for the english quote
+    db.getEnglish()
+    .then(() => {
+
+
+    // & te reo quotes table
+    db.getTeReo()
+
+    })
+    res.render('quoteview', viewBoth)
   })
