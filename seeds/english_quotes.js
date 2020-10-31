e0e1e792958670d3c6e36acb6d808bcb957d1bcf
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-10-31 15:14:49 +1300

    changed styling

--- seeds/english_quotes.js
+++ seeds/english_quotes.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('english').insert([
-        {id: 1001, author: 'Maori proverb', quote:'Unlike a canoe rope, a human bond cannot be severed.', foreign_id: 2001},
-        {id: 1002, author: 'Maori proverb', quote:'Seek the treasure you value most dearly. If you bow your head, let it be to a lofty mountain.', foreign_id: 2002},
-        {id: 1003, author: 'Maori proverb', quote:'A split tōtara tree is easy prey to the axe.', foreign_id: 2003},
+        {id: 1001, quote:'Unlike a canoe rope, a human bond cannot be severed.', foreign_id: 2001},
+        {id: 1002, quote:'Seek the treasure you value most dearly. If you bow your head, let it be to a lofty mountain.', foreign_id: 2002},
+        {id: 1003, quote:'A split tōtara tree is easy prey to the axe.', foreign_id: 2003},
         {id: 1004, author: 'Batman', quote:'I am Batman', foreign_id: 2004}
       ]);
     });
