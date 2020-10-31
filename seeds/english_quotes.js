badff34cc2cc5a4ad39fced05de81848da3e7bf4
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2020-10-31 15:10:12 +1300

    changed quotes to proverbs

--- seeds/english_quotes.js
+++ seeds/english_quotes.js
@@ -5,14 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('english').insert([
-        {id: 1001, author: 'Jerry Rice', quote:'Today I will do what others won’t, so tomorrow I can do what others can’t', foreign_id: 2001},
-        {id: 1002, author: 'Billie Jean King', quote:'Pressure is a privilege – it only comes to those who earn it.', foreign_id: 2002},
-        {id: 1003, author: 'Winnie the Pooh', quote:'You’re braver than you believe, stronger than you seem, and smarter than you think', foreign_id: 2003},
-        {id: 1004, author: 'Oscar Wilde', quote:'I can resist anything except temptation', foreign_id: 2004},
-        {id: 1005, author: 'Batman', quote:'I am Batman', foreign_id: 2005},
-        {id: 1006, author: 'Winston Churchill', quote:'Success is not final, failure is not fatal: it is the courage to continue that counts', foreign_id: 2006},
-        {id: 1007, author: 'Alexandre Dumas', quote:'Be good, be young, be true! Evil is nothing but vanity, let us have the pride of good, and above all let us never despair', foreign_id: 2007},
-        {id: 1008, author: 'Maya Angelou', quote:'Try to be a rainbow in someone elses cloud', foreign_id: 2008}
+        {id: 1001, author: 'Maori proverb', quote:'Unlike a canoe rope, a human bond cannot be severed.', foreign_id: 2001},
+        {id: 1002, author: 'Maori proverb', quote:'Seek the treasure you value most dearly. If you bow your head, let it be to a lofty mountain.', foreign_id: 2002},
+        {id: 1003, author: 'Maori proverb', quote:'A split tōtara tree is easy prey to the axe.', foreign_id: 2003},
+        {id: 1004, author: 'Batman', quote:'I am Batman', foreign_id: 2004}
       ]);
     });
 };
